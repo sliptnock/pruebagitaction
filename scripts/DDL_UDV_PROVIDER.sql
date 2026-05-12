a73ef82510df9d3a8ff85bff84548f1c7742723b
Alan Choquesillo <achoquesillo@gmail.com>
2026-05-12 01:54:13 -0500

    primer prueba gitaction v2

--- scripts/DDL_UDV_PROVIDER.sql
+++ scripts/DDL_UDV_PROVIDER.sql
@@ -17,6 +17,7 @@ provider_id bigint comment '',
 provider_metastore_id string comment '',
 created_at timestamp comment ''
 )
+--
 --USING DELTA
 --LOCATION 'PRM_STORAGELOCATION/$PRM_AMB/bcp/silver/credicorp/metastoreprovider/data/Provider'
 --TBLPROPERTIES(
